2fe8e091a745fa616efcdc68e7bcd4fed7247980
guch111 <guchi_sysu@163.com>
2025-02-09 23:49:26 +0800

    Site updated: 2025-02-09 23:49:24

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/02/09/hello-world/","2025/02/09/hexo搭建过程遇到的一些问题/","2025/02/09/markdown-file-quick-start/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
